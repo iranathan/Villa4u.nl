69928cc8c52dd3909d56260faa072de7c9a5cfb2
ira <iranathan8@gmail.com>
2023-04-19 16:53:49 +0200

    villa list

--- villalist.php
+++ villalist.php
@@ -3,4 +3,10 @@
 $db = new SQLite3("villa.sqlite");
 
 // get first 10 villas
-$villas = $db->querySingle("SELECT * FROM villa LIMIT 10;");
+$villas = $db->query("SELECT * FROM villa LIMIT 10;");
+
+if(isset($villas)) {
+    require("villalist.view.php");
+} else {
+    echo "No villas";
+}
--- villalist.view.php
+++ villalist.view.php
@@ -5,9 +5,52 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/headerfooter.css">
+    <link rel="stylesheet" href="./css/villalist.css">
+    <title>Villa4u</title>
 </head>
 <body>
-
+    <header>
+        <img src="image/lightlogosmall.png">
+        <nav>
+            <a>Villa's</a>
+            <a>Contact</a>
+        </nav>
+    </header>
+    <main>
+        <div id="villas">
+            <div class="villa">
+                <div class="villa-title">
+                    <h1>Villa</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur doloremque, dolores eius illum ipsum minima officiis quo voluptatibus. Harum possimus reiciendis repellendus suscipit tempora.</p>
+                </div>
+                <div class="villa-thumbnail">
+                    <img src="image/unnamed.png" alt="villa">
+                </div>
+            </div>
+        </div>
+        <?php while($res = $villas->fetchArray()) { ?>
+            <div id="villas">
+                <div class="villa">
+                    <div class="villa-title">
+                        <h1><?= $res['name'] ?></h1>
+                        <p><?= $res['description'] ?></p>
+                    </div>
+                    <div class="villa-thumbnail">
+                        <img src="image/unnamed.png" alt="villa">
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </main>
+    <footer>
+        <h2>Contact</h2>
+        <div id="footer-info">
+            <p>Telefoon: <br>06-12345678</p>
+            <p>E-Mail: <br>Villa4U@gmail.com</p>
+            <p>Adres: Rotterdam  <br>Heer Bokelweg 255 2414 AB </p>
+            <p>Sociale Media: <br>@Villa4U</p>
+        </div>
+    </footer>
 </body>
 </html>
