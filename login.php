41c6aa93ffe01971aba8819a148aaa432cb2c3ed
you@example.com <you@example.com>
2023-04-20 14:25:03 +0200

    start on login

---
+++ login.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/headerfooter.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
+    <title>Villa4u</title>
+</head>
+<body>
+    <header>
+        <img src="image/lightlogosmall.png">
+        <nav>
+            <a><strong>Villa's</strong></a>
+            <a><strong>Contact</strong></a>
+            <a><strong>Login</strong></a>
+        </nav>
+    </header>
+    <main>
+        <form id="login">
+            
+        </form>
+    </main>
+    <footer>
+        <h2>Contact</h2>
+        <div id="footer-info">
+            <p>Telefoon: <br>06-12345678</p>
+            <p>E-Mail: <br>Villa4U@gmail.com</p>
+            <p>Adres: Rotterdam  <br>Heer Bokelweg 255 2414 AB </p>
+            <p>Sociale Media: <br>@Villa4U</p>
+        </div>
+    </footer>
+</body>
+</html>
