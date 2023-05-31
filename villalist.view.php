60e9ce729748a7dc9a638c38b328a159e9558221
annnnnev <89993@glr.nl>
2023-05-31 14:15:11 +0200

    link edits

--- villalist.view.php
+++ villalist.view.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./css/headerfooter.css">
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon"
     <link rel="stylesheet" href="./css/villalist.css">
     <title>Villa4u</title>
 </head>
